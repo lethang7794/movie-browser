c3253825b23715fa954108cc81685090045f5927
Thang Le Quang <lethang7794@gmail.com>
2021-01-16 16:13:47 +0700

    Add description for each movie list page, reformat page info section

--- src/components/pages/MovieListPage.jsx
+++ src/components/pages/MovieListPage.jsx
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from 'react';
+import { Col, Row } from 'react-bootstrap';
 
 const API_KEY = process.env.REACT_APP_TMDB_API_KEY;
 const API_URL = process.env.REACT_APP_TMDB_API_URL;
@@ -6,9 +7,12 @@ const API_URL = process.env.REACT_APP_TMDB_API_URL;
 const pages = {
   top_rated: {
     title: 'Top Rated Movies',
+    description: `Quae aut reiciendis et. Qui ea enim est ad nihil beatae laboriosam laudantium veritatis. Quia voluptates excepturi officia. Sit necessitatibus nihil ratione odio a incidunt dolores fugiat.`,
   },
   now_playing: {
     title: 'Now Playing Movies',
+    description:
+      'Quis dolorum sequi quo magnam. Non fugiat neque unde fuga deserunt qui. Voluptas quasi fugiat illum est voluptatem harum voluptas fugit. Voluptas sit eius omnis.',
   },
 };
 
@@ -43,9 +47,16 @@ const MovieListPage = ({ type }) => {
 
   return (
     <div>
-      <h1 className='text-center'>
-        {pages[endpoint] && pages[endpoint].title}
-      </h1>
+      <Row className='MovieListInfo p-2 flex-column justify-content-center align-items-center'>
+        <Col lg={6}>
+          <h1 className='text-center'>
+            {pages[endpoint] && pages[endpoint].title}
+          </h1>
+          <p className='text-center'>
+            {pages[endpoint] && pages[endpoint].description}
+          </p>
+        </Col>
+      </Row>
       <ul>
         {movies.map((movie) => (
           <li key={movie.id}>{movie.title}</li>
